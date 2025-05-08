44deb526883f4274e7c97284308e518942dbbfcc
youpingh <34691387+youpingh@users.noreply.github.com>
2025-05-08 17:38:29 -0600

    test footer onclick

--- scripts/Loader.js
+++ scripts/Loader.js
@@ -1,10 +1,4 @@
 
-// import { AllWords } from './AllWords.js';
-// import { CSSManager } from './CSSManager.js';
-// import { Home } from '../home/Home.js';
-// import { Character } from '../character/Character.js';
-// import { Practice } from '../practice/Practice.js';
-
 /**
  * This is loader utility to load a content page based on the value of the window.location.hash.
  */
@@ -23,12 +17,9 @@ export class Loader {
    * Loads a blog according to the value of window.location.hash.
    */
   static async loadPage() {
-    // const loader = Loader.getInstance();
-    // let pageName = loader.initPageInfo();
-    // console.log('page name:', pageName);
 
     // the full window.location.hash looks like this #blogger/label/title
-    let pageName = window.location.hash.replace("#", "") || 'index.html';
+    let pageName = (window.location.hash.replace("#", "") || 'home/home') + '.html';
     let page = document.getElementById('page-content');
 
     try {
@@ -41,17 +32,4 @@ export class Loader {
       console.error(err);
     }
   }
-
-  /**
-   * Initializes the page information
-   * @returns page name to be loaded
-   */
-  // initPageInfo() {
-  //   // the full window.location.hash looks like this #label-name/page-title
-  //   let hash = window.location.hash.replace("#", "") || 'index.html';
-  //   hash = decodeURIComponent(hash);
-  //   let names = hash.split('/');
-  //   let pageName = names[0];
-  //   return pageName;
-  // }
 }
--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,10 @@ import { Loader } from './Loader.js';
  */
 window.addEventListener("hashchange", Loader.loadPage);
 window.addEventListener("DOMContentLoaded", Loader.loadPage);
+
+// add a hideMenuRows event to the document to hide the category menu when click on anywhere of the page.
+let footer = document.getElementById('page-footer');
+footer.addEventListener("click", () => {
+    window.location.hash = '#home/home';
+});
+
