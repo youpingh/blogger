cb7120ead423c55fea8223dc02f0635a46d70418
youpingh <34691387+youpingh@users.noreply.github.com>
2025-08-31 21:49:44 -0600

    new post

--- scripts/AllPosts.js
+++ scripts/AllPosts.js
@@ -431,6 +431,17 @@ export class AllPosts {
         "iframe": "<iframe src='https://youpingh.github.io/blogger/#afanti2/嬉笑怒骂/小作文-2' style='width:600px; height:1280px;' frameborder='0'></iframe>"
       },
       {
+        "title": "小作文-3",
+        "titleEnglish": "Essay-3",
+        "show": true,
+        "src": "https://youpingh.github.io/blogger/#afanti2/嬉笑怒骂/小作文-3",
+        "width": 600,
+        "height": 1320,
+        "intro": "鸟儿问答",
+        "introEnglish": "",
+        "iframe": "<iframe src='https://youpingh.github.io/blogger/#afanti2/嬉笑怒骂/小作文-3' style='width:600px; height:1280px;' frameborder='0'></iframe>"
+      },
+      {
         "title": "路边的洋娃娃",
         "titleEnglish": "Roadside Doll",
         "show": true,
